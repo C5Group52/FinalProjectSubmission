66b3ec87541e59cd6bbdca282fb4d96c3cb39d34
Paul Masamvu <p.masamvu@alustudent.com>
2026-07-31 12:33:17 +0200

    corrected profile in repo

--- lib/features/onboarding/data/repositories/profile_repository_impl.dart
+++ lib/features/onboarding/data/repositories/profile_repository_impl.dart
@@ -2,7 +2,7 @@ import '../../domain/entities/profile.dart';
 import '../../domain/repositories/profile_repository.dart';
 import '../datasources/profile_remote_data_source.dart';
 import '../models/profile_model.dart';
-
+//
 class ProfileRepositoryImpl implements ProfileRepository {
   ProfileRepositoryImpl(this._remote);
 
